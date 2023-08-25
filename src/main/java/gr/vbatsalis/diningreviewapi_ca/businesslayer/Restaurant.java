2c8b4c6446a959ab5e536003990524963abf914f
Evangelos Batsalis <vbatsalis@gmail.com>
2023-08-25 19:18:52 +0300

    add restaurant entity

---
+++ src/main/java/gr/vbatsalis/diningreviewapi_ca/businesslayer/Restaurant.java
@@ -0,0 +1,32 @@
+package gr.vbatsalis.diningreviewapi_ca.businesslayer;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+//Lombok annotations
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+//JPA-Entity annotations
+@Entity
+@Table(name="RESTAURANT")
+
+
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    ArrayList<ArrayList<Integer>> listScore = new
+
+
+}
