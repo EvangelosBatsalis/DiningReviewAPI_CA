48f4ae496f24275dd72f7d76e2fcaa4c468bb80e
Evangelos Batsalis <vbatsalis@gmail.com>
2023-08-29 11:18:41 +0300

    add sql tables and add entity parameters

--- src/main/java/gr/vbatsalis/diningreviewapi_ca/businesslayer/DiningReview.java
+++ src/main/java/gr/vbatsalis/diningreviewapi_ca/businesslayer/DiningReview.java
@@ -1,13 +1,34 @@
 package gr.vbatsalis.diningreviewapi_ca.businesslayer;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class DiningReview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name ="name", nullable = false)
     private String name;
-
-    private Restaurant restaurant;
-
-    public DiningReview(String name, Long id){
-        this.name = name;
-        this.restaurant = new Restaurant(id);
-    }
+    @Column(name ="peanutScore", nullable = true)
+    private int peanutScore;
+    @Column(name ="eggScore", nullable = true)
+    private int eggScore;
+    @Column(name ="dairyScore", nullable = true)
+    private int dairyScore;
+    @Column(name ="comment", nullable = true)
+    private String comment;
 
 }
--- src/main/java/gr/vbatsalis/diningreviewapi_ca/businesslayer/Restaurant.java
+++ src/main/java/gr/vbatsalis/diningreviewapi_ca/businesslayer/Restaurant.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 
 //Lombok annotations
 @Data
@@ -14,16 +13,37 @@ import java.util.ArrayList;
 
 //JPA-Entity annotations
 @Entity
-@Table(name="RESTAURANT")
+@Table(name = "RESTAURANT")
 
 
 public class Restaurant {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id", nullable = false)
+    @Column(name = "id", nullable = false)
     private Long id;
-    
 
+    @Column(name = "peanutIndividualScore", nullable = true)
+    private int peanutIndividualScore;
+    @Column(name = "peanutTotalScore", nullable = true)
+    private int peanutTotalScore;
+    @Column(name = "peanutAverageScore", nullable = true)
+    private float peanutAverageScore;
 
+    @Column(name = "eggIndividualScore", nullable = true)
+    private int eggIndividualScore;
+    @Column(name = "eggTotalScore", nullable = true)
+    private int eggTotalScore;
+    @Column(name = "eggAverageScore", nullable = true)
+    private float eggAverageScore;
+
+    @Column(name = "dairyIndividualScore", nullable = true)
+    private int dairyIndividualScore;
+    @Column(name = "dairyTotalScore", nullable = true)
+    private int dairyTotalScore;
+    @Column(name = "dairyAverageScore", nullable = true)
+    private float dairyAverageScore;
+
+    @Column(name = "totalAverageScore", nullable = true)
+    private float totalAverageScore;
 }
